90cc5402e02b5d4d0eeeb1a06e65f3e222e388a8
molach <moshe.lach@yahoo.com>
2026-01-01 13:54:09 -0500

    class 12/31

---
+++ src/O_bst/our/NavigableSet264.java
@@ -0,0 +1,6 @@
+package O_bst.our;
+
+public interface NavigableSet264<E> extends SortedSet264<E> {
+    E higher(E e);
+    E lower(E e);
+}
---
+++ src/O_bst/our/SortedSet264.java
@@ -0,0 +1,8 @@
+package O_bst.our;
+
+import M_hash_tables.A_set.B_our.Set264;
+
+public interface SortedSet264<E> extends Set264<E> {
+    E getFirst();
+    E getLast();
+}
